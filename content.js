9920d9239cb4a1c23b90534fa77c6192601bf9fa
Alex.D <alxcorp.pro@gmail.com>
2026-01-04 15:55:02 +0100

    update-carte

--- content.js
+++ content.js
@@ -7,21 +7,43 @@
         console.log('--- Initialisation de la Carte Alexandre Dubois ---');
         this.config = config;
 
+        this.creerBandeauEtudes();
         this.creerTexteHaut();
         this.creerReseauxBas();
         this.creerCercleCote();
       },
 
+      creerBandeauEtudes: function() {
+        const bandeau = document.createElement('a-text');
+        bandeau.setAttribute('value', 'BUT MMI 3ème année, 2025 - 2026');
+        bandeau.setAttribute('align', 'center');
+        bandeau.setAttribute('color', '#2b3d50');
+        bandeau.setAttribute('width', '8');
+        bandeau.setAttribute('rotation', '-90 0 0');
+        bandeau.setAttribute('position', '-0.8 0 -1.9');
+
+        this.el.appendChild(bandeau);
+      },
+
       creerTexteHaut: function() {
+        const { assets } = this.config;
         const groupeTexte = document.createElement('a-entity');
         groupeTexte.setAttribute('position', '-0.8 0 -1.2');
 
+        const logoNom = document.createElement('a-plane');
+        logoNom.setAttribute('src', assets.logos.main);
+        logoNom.setAttribute('height', '0.6');
+        logoNom.setAttribute('width', '0.6');
+        logoNom.setAttribute('rotation', '-90 0 0');
+        logoNom.setAttribute('position', '-0.9 0 0');
+
         const texte = document.createElement('a-text');
         texte.setAttribute('value', 'ALEXANDRE DUBOIS');
-        texte.setAttribute('align', 'center');
+        texte.setAttribute('align', 'left');
         texte.setAttribute('color', '#333333');
         texte.setAttribute('width', '6');
         texte.setAttribute('rotation', '-90 0 0');
+        texte.setAttribute('position', '-0.2 0 0');
         texte.setAttribute('animation', {
           property: 'position',
           from: '0 -0.5 0',
@@ -30,6 +52,7 @@
           easing: 'easeOutQuad'
         });
 
+        groupeTexte.appendChild(logoNom);
         groupeTexte.appendChild(texte);
         this.el.appendChild(groupeTexte);
       },
@@ -37,7 +60,7 @@
       creerReseauxBas: function() {
         const { assets, links } = this.config;
         const groupeReseaux = document.createElement('a-entity');
-        groupeReseaux.setAttribute('position', '-0.8 0 1.2');
+        groupeReseaux.setAttribute('position', '-0.8 0 1.6');
 
         const reseaux = [
           { x: -0.5, assetId: assets.logos.linkedin, url: links.linkedin },
